a6eff188a352fd070c0912572efe209054693123
pawan10 <pawandev1099@gmail.com>
2021-05-29 21:24:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
         <Profile />
       </Route>
     </BrowserRouter>
-
   );
 }
 
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -41,6 +41,6 @@ const Home = () => {
             </div>
         </div>
     )
-}
+    }
 
 export default Home
